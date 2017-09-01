d5851682447cd26749c70d74593122d0ba6706a9
LinuxPlus <linuxplus@LinuxPluss-Mac.local>
2017-09-01 11:01:28 -0700

    my second commit

--- TableViewController.swift
+++ TableViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    var animal = ["Dog","Cat","Elephant","Cow","Goat"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.title = "My tableView"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title:"Insert", style: .plain,target:self, action: "insert")
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    func insert(){
+        animal.append("item")
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,43 +35,49 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return animal.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = animal[indexPath.row]
         return cell
     }
-    */
+    
 
-    /*
-    // Override to support conditional editing of the table view.
+   
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+        
         return true
     }
-    */
+    
 
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            animal.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            print("kashee")
+        }
+        tableView.reloadData()
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+            if cell?.accessoryType == .checkmark{
+                cell?.accessoryType = .none
+            }else{
+              cell?.accessoryType = .checkmark
+        }
+    }
+ 
 
     /*
     // Override to support rearranging the table view.
